153ff94dade78276eecc134c3a71ad3080d0a6bb
jasmynejean-remy <jasmynej14@gmail.com>
2024-12-01 22:38:58 -0500

    search box

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+        <div className="header">
+            <h1>Vacation Planner</h1>
+        </div>
+
+        <div className="search-box">
+            <form>
+                <div className="search-row">
+                    <label htmlFor="weather">Preferred Weather:</label>
+                    <select id="weather">
+                        <option value="Warm">Warm</option>
+                        <option value="Cool">Cool</option>
+                        <option value="Snowy">Snowy</option>
+                    </select>
+
+                    <label htmlFor="activity">Activity Type:</label>
+                    <select id="activity">
+                        <option value="Adventure">Adventure</option>
+                        <option value="Relaxation">Relaxation</option>
+                        <option value="Culture">Culture</option>
+                    </select>
+                </div>
+                <div className="search-row">
+                    <label htmlFor="budget">Budget: </label>
+                    <input type="range" min="1" max="10" id="budget" />
+                </div>
+                <button id="search">Get A Vacation</button>
+            </form>
+
+        </div>
+    </div>
   )
 }
 
